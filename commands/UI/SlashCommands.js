59f08f1e6d3bc015d40c4456c9da618fd9e77ae1
ddibiyong <ddibiyong@gmail.com>
2025-04-18 15:30:26 +0900

    Update SlashCommands.js

--- commands/UI/SlashCommands.js
+++ commands/UI/SlashCommands.js
@@ -1,2 +1,12 @@
 // 이 부분은 UI를 구성하는 부분입니다.
 // discord.js의 slash command를 사용하여 UI를 구성합니다.
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("앙기모뚜")
+    .setDescription("앙기모뚜"),
+  async execute(interaction) {
+    await interaction.reply("똥");
+  },
+};
